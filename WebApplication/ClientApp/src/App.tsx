d8c07bb026d333aceee070190c9cced76dadc3cf
Lucas Stirpe <v-lstirpe@youngliving.com>
2019-09-17 18:06:31 -0300

    New theme

--- WebApplication/ClientApp/src/App.tsx
+++ WebApplication/ClientApp/src/App.tsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { Product } from './entities/product';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Button } from 'react-bootstrap';
-// import Button from 'react-bootstrap/Button';
 import { Button } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import SearchAppBar from './toolbar';
 import Icon from '@material-ui/core/Icon';
+import TextField from '@material-ui/core/TextField';
+import DeleteIcon from '@material-ui/icons/Delete';
+import UpdateIcon from '@material-ui/icons/Update';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 interface IState {
  listOfProducts : Product[];
@@ -45,7 +53,7 @@ class App extends Component<IProps, IState> {
 
   ShowForm(){
     document.getElementById("divAdd")!.style.display = "block"; //The ! means "trust me, this is not a null reference"
-    this.setState({buttonName : "Hide form"}); 
+    this.setState({buttonName : "Cancel"}); 
     this.setState({isHidden : true});
   }
   
@@ -58,52 +66,49 @@ class App extends Component<IProps, IState> {
 
   render() {
     return (
-      <div>
-        <h3 className="text-center info">List of products</h3>
-          <table className="text-center table table-dark">    
-              <thead>    
-              <tr>
-                {/* <th>Id</th> */}
-                <th>Name</th>
-                <th>Price</th>
-              </tr>  
-              </thead> 
-              <tbody>
-              {
-                this.state.listOfProducts.map(product => (
-                  <tr key= {`${product.name}_${product.price}`}> 
-                {/* <td>{product.productId} </td> */}
-                    <td >{product.name} </td>
-                    <td> {product.price} </td>
-                    <td> <Button variant="contained" color="default"> Update</Button> </td>
-                    <td> <Button variant="contained" color="secondary"> Delete </Button></td>
-                  </tr>
-                ))           
-              }
-              </tbody>
-          </table>
+      <React.Fragment>
+        <SearchAppBar></SearchAppBar>
+        <DialogTitle>List of products</DialogTitle>
 
-          <br/>
-          <br/>
+        <Table size="medium">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Price</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.listOfProducts.map(product => (
+              <TableRow key= {`${product.name}_${product.price}`}>
+                <TableCell>{product.name} </TableCell>
+                <TableCell>{product.price}</TableCell>
+                <TableCell><Button variant="contained" color="default">Update<UpdateIcon/></Button></TableCell>
+                <TableCell><Button variant="contained" color="secondary">Delete<DeleteIcon/></Button>  </TableCell>
+              </TableRow>
+            ))}
+            </TableBody>
+        </Table>
 
-          {!this.state.isHidden ? (
-            <Button variant="contained" color="primary" id="buttonAdd" onClick={() => this.ShowForm()}> {this.state.buttonName} </Button> 
-            ) : (
-            <Button variant="contained" color="primary" id="buttonAdd" onClick={() => this.HideForm()}> {this.state.buttonName} </Button> 
-            )
-          }
+        <br/>
+        <br/>
 
-          <br/><br/>
-            <div id="divAdd">
-                <input type="text" name="inputName" placeholder="input the name" />
-                <br/><br/>
-                <input type="text" name="inputPrice" placeholder="input the price"/>
-                <br/><br/>
-                <button id="botonF1" onClick={this.AddProduct}>Save</button>
-            </div>
-      </div>
+        {!this.state.isHidden ? (
+          <Button variant="contained" color="primary" id="buttonAdd" onClick={() => this.ShowForm()}> {this.state.buttonName} <AddCircleIcon/> </Button> 
+          ) : (
+          <Button variant="contained" color="primary" id="buttonAdd" onClick={() => this.HideForm()}> {this.state.buttonName} </Button> 
+          )
+        }
+
+        <br/><br/>
+          <div id="divAdd">
+              <TextField label="Name" name="inputName" placeholder="input the name" />
+              <br/><br/>
+              <TextField label="Price" name="inputPrice" placeholder="input the price"/>
+              <br/><br/>
+              <Button variant="contained" color="default" onClick={this.AddProduct}>Save</Button>
+          </div>
+      </React.Fragment>
     );
   }
 }
 export default App;
-
